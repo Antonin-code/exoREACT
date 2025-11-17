61d68e97d659cc37825cc88f87f9bbd3dc4c2e1f
Antonin Maubert <antoninmaubert224@gmail.com>
2025-11-17 19:13:55 +0100

    update

--- src/movieCard.jsx
+++ src/movieCard.jsx
@@ -1,10 +1,11 @@
 export default function MovieCard({ movies }) {
     return (
-        <div>
+        <div className="movieCards">
             {movies.map((movie) => (
                 <div key={movie.id}>
                     <h3>{movie.titre}</h3>
                     <p>{movie.year}</p>
+                    <img src={movie.posterUrl} alt={movie.titre} />
                     <p>{movie.desciption}</p>
                 </div>
             ))}
